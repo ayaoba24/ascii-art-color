68109c5c4b4a036b4fd1f6cd0f7911bfedc49d59
gamp <tomisin_adeniyi11@yahoo.com>
2026-03-12 09:56:04 +0000

    my work

--- main.go
+++ main.go
@@ -17,8 +17,14 @@ func main() {
 	case 1:
 		text = args[0]
 	case 2:
-		subString = args[0]
-		text = args[1]
+		if strings.HasPrefix(args[0], "--color=") {
+			colorName := strings.TrimPrefix(args[0], "--color=")
+			colorCode = branch.Color(colorName)
+			text = args[1]
+		} else {
+			subString = args[0]
+			text = args[1]
+		}
 	case 3:
 		if !strings.HasPrefix(args[0], "--color=") {
 			log.Fatal("Invalid arguments")
@@ -34,4 +40,22 @@ func main() {
 	banner := branch.Banner("standard.txt")
 	result := branch.AsciiArt(text, subString, colorCode, banner)
 	fmt.Print(result)
+}
+
+
+func getValidColor(colorName string) string {
+	validColors := []string{"black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"}
+
+	for {
+		colorCode := branch.Color(colorName)
+		if colorCode != "" {
+			return colorCode
+		}
+
+		fmt.Printf("NOT '%s' is not a valid color.\n", colorName)
+		fmt.Printf("Available colors: %s\n", strings.Join(validColors, ", "))
+		fmt.Print("Please enter a valid color: ")
+
+		fmt.Scan(&colorName)
+	}
 }
